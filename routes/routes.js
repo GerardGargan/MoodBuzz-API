7bdb815c8b19e0e5412e93d329b29755602c95f6
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-02-01 12:31:43 +0000

    set up database connection

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const db = require('./../util/dbconn');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    
     res.send('Hello World');
 });
 
