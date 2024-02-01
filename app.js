b4bde3a53fc716b37cfbf00d3791ab79a4593f38
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-02-01 12:27:06 +0000

    set up router, tested get for default route - working

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config({ path: 'config.env' });
+const router = require('./routes/routes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use('/', router);
 
 app.listen(PORT, (err) => {
     console.log(`API Server listening on PORT ${PORT}`);
