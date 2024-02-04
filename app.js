c7f6e7eec4bd648a018c8057e1ff559a396f5e4b
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-02-04 21:59:07 +0000

    added controller for view snapshot

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const router = require('./routes/routes');
 
-const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use('/', router);
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,82 +1,99 @@
-const db = require('./../util/dbconn');
+const db = require("./../util/dbconn");
 
 exports.getSnapshot = async (req, res) => {
-    
-    const { id } = req.params;
-    const userid = 4;
-    try {
-        const queryEmotions = `SELECT snapshot.snapshot_id, snapshot.user_id, note, date, time, snapshot_emotion_id, emotion, emotion.emotion_id, snapshot_emotion.rating FROM snapshot INNER JOIN snapshot_emotion ON snapshot.snapshot_id = snapshot_emotion.snapshot_id INNER JOIN emotion ON snapshot_emotion.emotion_id = emotion.emotion_id
+  const { id } = req.params;
+  const userid = 4; //hardcoded - needs to be sent through API headers
+
+  const queryEmotions = `SELECT snapshot.snapshot_id, snapshot.user_id, note, date, time, snapshot_emotion_id, emotion, emotion.emotion_id, snapshot_emotion.rating FROM snapshot INNER JOIN snapshot_emotion ON snapshot.snapshot_id = snapshot_emotion.snapshot_id INNER JOIN emotion ON snapshot_emotion.emotion_id = emotion.emotion_id
         WHERE snapshot.snapshot_id = ? AND user_id = ?`;
 
-        const vals = [id, userid];
-        const [rows, fielddata] = await db.query(queryEmotions, vals);
-        //console.log(rows);
+  const vals = [id, userid];
 
-        const groupedData = {};
+  try {
+    const [rows, fielddata] = await db.query(queryEmotions, vals);
 
-        rows.forEach(row => {
-            const { snapshot_id, user_id, note, date, time, snapshot_emotion_id, rating, emotion, emotion_id } = row;
-            if (!groupedData[snapshot_id]) {
-                groupedData[snapshot_id] = {
-                    snapshot_id,
-                    user_id,
-                    note,
-                    date: formatDatabaseDate(date),
-                    time,
-                    emotions: [],
-                }
-            }
-            groupedData[snapshot_id].emotions.push({ emotion: emotion, emotion_id: emotion_id, snapshot_emotion_id: snapshot_emotion_id, rating: rating, ratings: {} });
-        });
+    if (rows.length > 0) {
+      //snapshot exists, parse the data structure needed
+      const groupedData = {};
 
-        //get rating data for each emotion and insert it into the groupedData object, under each emotion as an array of ratings
-        for (const emotion of groupedData[id].emotions) {
-            const vals = [emotion.emotion_id];
-            const query = `SELECT rating, emotion.emotion_id, short_desc, long_desc FROM emotion INNER JOIN emotion_rating ON emotion.emotion_id = emotion_rating.emotion_id INNER JOIN rating ON emotion_rating.rating_id = rating.rating_id WHERE emotion.emotion_id = ?`;
-            const [rows, fielddata] = await db.query(query, vals);
-            rows.forEach(row => {
-                emotion.ratings[row.rating] = {rating: row.rating, short_desc: row.short_desc, long_desc: row.long_desc};
-            });
+      rows.forEach((row) => {
+        const {
+          snapshot_id,
+          user_id,
+          note,
+          date,
+          time,
+          snapshot_emotion_id,
+          rating,
+          emotion,
+          emotion_id,
+        } = row;
+        if (!groupedData[snapshot_id]) {
+          groupedData[snapshot_id] = {
+            snapshot_id,
+            user_id,
+            note,
+            date: formatDatabaseDate(date),
+            time,
+            emotions: {},
+          };
         }
+        groupedData[snapshot_id].emotions[emotion_id] = {
+          emotion: emotion,
+          emotion_id: emotion_id,
+          snapshot_emotion_id: snapshot_emotion_id,
+          rating: rating,
+          ratings: {},
+        };
+      });
 
-        //get the triggers for the snapshot
-        const triggerQuery = `SELECT trigger_name, icon, triggers.trigger_id FROM snapshot_trigger INNER JOIN triggers ON snapshot_trigger.trigger_id = triggers.trigger_id WHERE snapshot_id = ?`;
-        const [trigrows, field] = await db.query(triggerQuery, [id]);
-        console.log(trigrows);
-
-        
-        /*
-        groupedData[id].emotions.forEach(emotion => {
-            console.log(emotion.ratings);
-            //emotion.ratings.forEach(rating => {
-                //console.log(`${emotion.emotion_id} ${rating.rating} ${rating.short_desc}`);
-              //  console.log(rating);
-           // })
-        })
-        */
-       res.json({
-        status: 'success',
-        message: 'message',
-        result: {snapshot: groupedData[id], triggers: trigrows}
-       })
-    } catch (err) {
-        res.status(500);
-        res.json({
-            status: 'failure',
-            message: `${err}`
+      //get rating data for each emotion and insert it into the groupedData object, under each emotion as an array of ratings
+      for (const emotion of Object.values(groupedData[id].emotions)) {
+        const vals = [emotion.emotion_id];
+        const query = `SELECT rating, emotion.emotion_id, short_desc, long_desc FROM emotion INNER JOIN emotion_rating ON emotion.emotion_id = emotion_rating.emotion_id INNER JOIN rating ON emotion_rating.rating_id = rating.rating_id WHERE emotion.emotion_id = ?`;
+        const [rows, fielddata] = await db.query(query, vals);
+        rows.forEach((row) => {
+          emotion.ratings[row.rating] = {
+            rating: row.rating,
+            short_desc: row.short_desc,
+            long_desc: row.long_desc,
+          };
         });
+      }
+
+      //get the triggers for the snapshot
+      const triggerQuery = `SELECT trigger_name, icon, triggers.trigger_id, snapshot_trigger_id FROM snapshot_trigger INNER JOIN triggers ON snapshot_trigger.trigger_id = triggers.trigger_id WHERE snapshot_id = ?`;
+      const [trigrows, field] = await db.query(triggerQuery, [id]);
+      //console.log(trigrows);
+      
+      res.status(200);
+      res.json({
+        status: 'success',
+        message: `Snapshot ${id} retrieved`,
+        result: groupedData[id]
+      });
+
+    } else {
+      res.status(404);
+      res.json({
+        status: "failure",
+        message: `Invalid snapshot id ${id}`,
+      });
     }
-
-
-
-   
+  } catch (err) {
+    res.status(500);
+    res.json({
+      status: "failure",
+      message: `Error making API request: ${error}`,
+    });
+  }
 };
 
 function formatDatabaseDate(date) {
-    const databaseDate = new Date(date);
-    const year = databaseDate.getFullYear();
-    const month = databaseDate.getMonth() + 1; //zero indexed
-    const day = databaseDate.getDate();
+  const databaseDate = new Date(date);
+  const year = databaseDate.getFullYear();
+  const month = databaseDate.getMonth() + 1; //zero indexed
+  const day = databaseDate.getDate();
 
-    return `${day}/${month}/${year}`;
+  return `${day}/${month}/${year}`;
 }
