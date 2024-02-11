f8d9bb548efa6c05bc2a898d7fbcdc6f24304567
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-02-11 19:13:05 +0000

    added router and controller for emotions, added route for getting all snapshots by user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const snapshot_router = require('./routes/snapshot_routes');
 const user_router = require('./routes/user_routes');
+const emotion_router = require('./routes/emotion_routes');
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/snapshot', snapshot_router);
 app.use('/user', user_router);
+app.use('/emotions', emotion_router);
 
 module.exports = app;
